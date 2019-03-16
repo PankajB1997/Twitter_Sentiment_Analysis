98a73084f1aa40bfe3011f242216e5517c6d6a14
PankajB1997 <pankajbhootra@gmail.com>
2019-03-16 23:15:19 +0800

    Update README with total tweets count

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Twitter_Sentiment_Analysis
-
-CS4225 Project.
+# Twitter Sentiment Analysis  
+  
+CS4225 Project  
+  
+Total tweets added:  31128855 (3.01 GB)  
+  
+This is consolidated from multiple sources, including [an online twitter archive](https://archive.org/details/archiveteam-twitter-stream-2018-10), [Sentiment140 dataset on Kaggle](https://www.kaggle.com/kazanova/sentiment140) and around 40,000 tweets that we downloaded manually using the Twitter API.
--- prepare_tweets_csv.py
+++ prepare_tweets_csv.py
@@ -12,17 +12,17 @@ CSV_SAVEPATH = "tweets_large_32M.csv"
 
 size = 0
 
-with open(CSV_SAVEPATH, "a") as csvfile:
+with open(CSV_SAVEPATH, "a", encoding="utf-8") as csvfile:
 
     for path in PKL_PATHS:
         for pickle_filepath in os.listdir(path):
             tweets_current_pkl = pickle.load(open(os.path.join(path, pickle_filepath), "rb"))
-            pd.DataFrame(tweets_current_pkl).to_csv(csvfile, sep=",", header=False, index=False, encoding="ISO-8859-1")
+            pd.DataFrame(tweets_current_pkl).to_csv(csvfile, sep=",", header=False, index=False, encoding="utf-8")
             size += len(tweets_current_pkl)
 
     for path in CSV_PATHS:
         tweets_current_csv = pd.read_csv(path, header=None, index_col=None)
-        tweets_current_csv.to_csv(csvfile, sep=",", header=False, index=False, encoding="ISO-8859-1")
+        tweets_current_csv.to_csv(csvfile, sep=",", header=False, index=False, encoding="utf-8")
         size += tweets_current_csv.shape[0]
 
 print("Total tweets added: ", size)
