4bfcafa49fbfebad8bc03eaa00002da8d1c7661b
PankajB1997 <pankajbhootra@gmail.com>
2019-03-16 23:03:22 +0800

    Change encoding to ISO-8859-1

--- prepare_tweets_csv.py
+++ prepare_tweets_csv.py
@@ -17,12 +17,12 @@ with open(CSV_SAVEPATH, "a") as csvfile:
     for path in PKL_PATHS:
         for pickle_filepath in os.listdir(path):
             tweets_current_pkl = pickle.load(open(os.path.join(path, pickle_filepath), "rb"))
-            pd.DataFrame(tweets_current_pkl).to_csv(csvfile, sep=",", header=False, index=False, encoding="utf-8")
+            pd.DataFrame(tweets_current_pkl).to_csv(csvfile, sep=",", header=False, index=False, encoding="ISO-8859-1")
             size += len(tweets_current_pkl)
 
     for path in CSV_PATHS:
         tweets_current_csv = pd.read_csv(path, header=None, index_col=None)
-        tweets_current_csv.to_csv(csvfile, sep=",", header=False, index=False, encoding="utf-8")
+        tweets_current_csv.to_csv(csvfile, sep=",", header=False, index=False, encoding="ISO-8859-1")
         size += tweets_current_csv.shape[0]
 
 print("Total tweets added: ", size)
