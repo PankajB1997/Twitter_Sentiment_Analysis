8f7c34f54addae83983999e4644ac9c748b47ad9
RonaldLWM <youcanfindronald@gmail.com>
2019-03-22 11:10:03 +0800

    Created initial ML script with tfidf standardscaler and randomforest

--- preprocessor/build.sbt
+++ preprocessor/build.sbt
@@ -1,3 +1,4 @@
+
 name := "TwitterSentimentAnalysis"
 
 version := "0.1"
@@ -5,3 +6,5 @@ version := "0.1"
 scalaVersion := "2.11.0"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0"
--- preprocessor/src/main/scala/SentimentAnalysis.scala
+++ preprocessor/src/main/scala/SentimentAnalysis.scala
@@ -49,5 +49,71 @@ object SentimentAnalysis {
     //   println(result)
     //   count = count + 1
     // }
+
+    // Ronald
+
+    val spark = SparkSession.builder.
+      master("local")
+      .appName("example")
+      .getOrCreate()
+
+    // Insert csv filename here
+    val df = spark.read.format("csv").option("header", "true").load("test.csv")
+    //    val df = spark.read.csv("file:///Users/ronaldlim/Downloads/trainingandtestdata/training.1600000.processed.noemoticon.csv").toDF("label", "col2", "col3", "col4", "col5", "text")
+    val Array(trainingData, testData) = df.randomSplit(Array(0.7, 0.3))
+
+    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
+    val hashingTF = new HashingTF().setNumFeatures(1000).setInputCol("words").setOutputCol("rawFeatures")
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features").setMinDocFreq(0)
+    val labelIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      .fit(df)
+    val rf = new RandomForestClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("scaledFeatures")
+      .setNumTrees(100)
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(labelIndexer.labels)
+    val scaler = new StandardScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setWithStd(true)
+      .setWithMean(false)
+
+
+    //      val wordsData = tokenizer.transform(df)
+    //
+    //      val featurizedData = hashingTF.transform(wordsData)
+    //
+    //      val idfModel = idf.fit(featurizedData)
+    //
+    //      val rescaledData = idfModel.transform(featurizedData)
+    //      rescaledData.select("label", "features").show()
+    //
+    //      val scalerModel = scaler.fit(rescaledData)
+    //
+    //      // Normalize each feature to have unit standard deviation.
+    //      val scaledData = scalerModel.transform(rescaledData)
+    //      scaledData.show()
+
+
+    val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, idf, labelIndexer, scaler, rf, labelConverter))
+
+    val model = pipeline.fit(trainingData)
+
+    // Make predictions.
+    val predictions = model.transform(testData)
+    //    predictions.select("predictedLabel", "label", "scaledFeatures").show(5)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - accuracy))
   }
 }
